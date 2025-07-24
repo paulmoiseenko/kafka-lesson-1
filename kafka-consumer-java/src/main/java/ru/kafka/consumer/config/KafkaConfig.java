a6deac92892d256a060efbac8f72cb0a9f5d2942
Paul Moiseenko <cr7mks1@mail.ru>
2025-07-24 21:53:28 +0300

    add docker file for consumer-java

--- kafka-consumer-java/src/main/java/ru/kafka/consumer/config/KafkaConfig.java
+++ kafka-consumer-java/src/main/java/ru/kafka/consumer/config/KafkaConfig.java
@@ -1,15 +1,18 @@
 package ru.kafka.consumer.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.util.Properties;
 
+@Slf4j
 public class KafkaConfig {
 
     public static Properties getConsumerProps() {
         Properties props = new Properties();
-        String bootstrapServers = System.getProperty("kafka.bootstrap.servers", "localhost:9092");
+        String bootstrapServers = System.getenv().getOrDefault("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092");
+        log.info("Kafka bootstrap servers: {}", bootstrapServers);
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
